9567f360922693a2f3f38586d0a684def93d3402
Baptiste Jacquin <baptiste.jacquin@campus-numerique-in-the-alps.com>
2020-01-06 14:30:52 +0100

    ajout de la plateforme

--- script/realGame.js
+++ script/realGame.js
@@ -14,16 +14,32 @@ let config = {
         }
     }
 };
+let game = new Phaser.Game(config);
+let paddle;
+let cursors;
 
 
 function preload() {
-
+    this.load.image('paddle', 'assets/mexico.png');
 }
 
 function create() {
+    this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor("#3498db");
+    cursors = this.input.keyboard.createCursorKeys();
+    paddle = this.physics.add.sprite(window.innerWidth - (window.innerWidth / 2), window.innerHeight - 10, "paddle");
+    paddle.enableBody = true;
+    paddle.body.immovable = true;
+    paddle.body.collideWorldBounds = true;
 
 }
 
 function update() {
+    if (cursors.left.isDown) {
+        paddle.body.velocity.x = -800;
+    } else if (cursors.right.isDown ) {
+        paddle.body.velocity.x = 800;
+    } else {
+        paddle.body.velocity.x = 0;
+    }
 
 }
